12104920ad8aaca300acd628573ccb524c4e85f4
rupesh <rpiwal02@gmail.com>
2024-09-29 14:00:56 +0530

    designed metrices component

--- src/components/MetricsCard.tsx
+++ src/components/MetricsCard.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-
-const MetricsCard = () => {
-  return <div>MetricsCard</div>;
+interface MetricsCardProps {
+  title: string;
+  value: number | string;
+  icon: JSX.Element;
+}
+const MetricsCard: React.FC<MetricsCardProps> = ({ title, value, icon }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4 flex items-center justify-between">
+      <div>
+        <h3 className="text-sm text-gray-500">{title}</h3>
+        <p className="text-xl font-semibold">{value}</p>
+      </div>
+      <div className="text-3xl text-blue-500">{icon}</div>
+    </div>
+  );
 };
 
 export default MetricsCard;
